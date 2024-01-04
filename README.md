e71d7cb708aebaa2dc2c5fad8e38231c932a300a
Mastereye7 <91390859+Mastereye7@users.noreply.github.com>
2024-01-04 06:56:05 +0100

    1.1.0 Added !randomboss. Added mod/broadcaster check

--- README.md
+++ README.md
@@ -6,6 +6,48 @@ Bot created to listen to and integrate with Twitch chat.
 
 An in-depth paragraph about your project and overview of use.
 
+## Features
+
+- Commands called in chat with '!'
+- Checks whether the caller is a broadcaster or a moderator.
+
+## Available commands
+
+* !dice - Picks a random number from 1-6
+* !randomboss - Picks a random Genshin world boss from a static list. 
+
+### !randomboss
+List of potential bosses:
+        'Anemo Hypostasis',
+        'Cryo Hypostasis',
+        'Cryo Regisvine',
+        'Electro Hypostasis',
+        'Geo Hypostasis',
+        'Oceanid',
+        'Primo Geovishap',
+        'Pyro Regisvine',
+        'Ruin Serpent',
+        'Bathysmal Vishap Herd',
+        'Golden Wolflord',
+        'Hydro Hypostasis',
+        'Maguu Kenki',
+        'Perpetual Mechanical Array',
+        'Pyro Hypostasis',
+        'Thunder Manifestation',
+        'Jadeplume Terrorshroom',
+        'Electro Regisvine',
+        'Aeonblight Drake',
+        'Algorithm of Semi-Intransient Matrix of Overseer Network',
+        'Dendro Hypostasis',
+        'Setekh Wenut',
+        'Iniquitous Baptist',
+        'Icewind Suite: Dirge of Coppelia',
+        'Icewind Suite: Nemesis of Coppelius',
+        'Emperor of Fire and Iron',
+        'Experimental Field Generator',
+        'Millennial Pearl Seahorse',
+        'Hydro Tulpa'
+
 ## Getting Started
 
 ### Dependencies
@@ -17,6 +59,8 @@ An in-depth paragraph about your project and overview of use.
 
 * How/where to download your program
 * Any modifications needed to be made to files/folders
+
+- Make sure to create a /config folder with a default.json file to run the software.
 
 ### Executing program
 
@@ -41,6 +85,9 @@ ex. [@Mastereye7](https://twitter.com/Mastereye7)
 
 ## Version History
 
+* 1.1
+    * Added !randomboss command which randomises a random Genshin world boss
+    * Checks whether message sender is the broadcaster or a moderator
 * 1.0
     * Various bug fixes and optimizations
     * See [commit change]() or See [release history]()
---
+++ bot.js
@@ -0,0 +1,103 @@
+const tmi = require('tmi.js');
+const config = require('config');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: config.get('username'),
+    password: config.get('password')
+  },
+  channels: config.get('channels')
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, userState, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+  if (msg[0] !== '!') { return }
+
+  const isBroadcaster = userState.badges !== null && userState.badges.broadcaster !== null;
+  const isModerator = userState.mod !== null && userState.mod;
+  console.log(`isBroadcaster: ${isBroadcaster}`);
+  console.log(`isModerator: ${isModerator}`);
+
+  if (!isBroadcaster && !isModerator) { return; }
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else if (commandName === '!randomboss') {
+
+    const boss = randomWorldBoss();
+    client.say(target, `${boss}`)
+    console.log(`* Executed ${commandName} command`);
+  }
+  else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Function that returns a random world boss
+function randomWorldBoss () {
+    const bosses = [
+        'Anemo Hypostasis',
+        'Cryo Hypostasis',
+        'Cryo Regisvine',
+        'Electro Hypostasis',
+        'Geo Hypostasis',
+        'Oceanid',
+        'Primo Geovishap',
+        'Pyro Regisvine',
+        'Ruin Serpent',
+        'Bathysmal Vishap Herd',
+        'Golden Wolflord',
+        'Hydro Hypostasis',
+        'Maguu Kenki',
+        'Perpetual Mechanical Array',
+        'Pyro Hypostasis',
+        'Thunder Manifestation',
+        'Jadeplume Terrorshroom',
+        'Electro Regisvine',
+        'Aeonblight Drake',
+        'Algorithm of Semi-Intransient Matrix of Overseer Network',
+        'Dendro Hypostasis',
+        'Setekh Wenut',
+        'Iniquitous Baptist',
+        'Icewind Suite: Dirge of Coppelia',
+        'Icewind Suite: Nemesis of Coppelius',
+        'Emperor of Fire and Iron',
+        'Experimental Field Generator',
+        'Millennial Pearl Seahorse',
+        'Hydro Tulpa'
+    ];
+
+    const randomNumber = Math.floor(Math.random() * bosses.length);
+
+    const chosenBoss = bosses[randomNumber];
+    return chosenBoss;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
